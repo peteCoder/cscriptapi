aaec47f1402eb71b6c0598660b5b9616a7d25e0a
peteCoder <petercodercoder@gmail.com>
2024-01-15 17:08:15 -0800

    included static files and renamed admin portal

--- base/admin.py
+++ base/admin.py
@@ -5,6 +5,10 @@ from .models import (
     Wallet
 )
 
+admin.site.site_header = "Coinchip Administration"
+admin.site.site_title = "Coinchip Admin Portal"
+admin.site.index_title = "Welcome to Coinchip Portal"
+
 # Register your models here.
 admin.site.register(Transactions)
 admin.site.register(Comments)
